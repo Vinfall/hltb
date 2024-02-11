8faf1d79e3d4a14b3c049ff75eb7f856912978ef
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-02-11 16:35:05 +0800

    sanitizer: make post_sanitize a function

--- HLTB-Sanitizer.py
+++ HLTB-Sanitizer.py
@@ -44,83 +44,90 @@ def determine_status(row):
     return key
 
 
+def post_sanitize(df):
+    # Convert to time type
+    df[["Progress", "Main Story", "Main + Extras", "Completionist"]] = df[
+        ["Progress", "Main Story", "Main + Extras", "Completionist"]
+    ].apply(pd.to_timedelta, errors="coerce")
+    # Exclude NaN line
+    df = df.dropna(
+        subset=["Progress", "Main Story", "Main + Extras", "Completionist"], how="all"
+    )
+    # Choose the maximum one
+    max_playtime = df[["Progress", "Main Story", "Main + Extras", "Completionist"]].max(
+        axis=1
+    )
+    # Convert back to string as "Playtime"
+    max_playtime_hours = max_playtime.dt.total_seconds().div(3600)
+    df["Playtime"] = max_playtime_hours.apply(
+        lambda x: "{:02d}:{:02d}:{:02d}".format(
+            int(x // 1), int((x % 1) * 60), int((x * 60) % 60)
+        )
+    )
+
+    # Use "Added" column as "Date"
+    df["Added"] = pd.to_datetime(
+        df["Added"], format="%Y-%m-%d %H:%M:%S", errors="coerce"
+    )
+    df["Date"] = df["Added"].dt.strftime("%Y-%m-%d")
+
+    # Choose nearest date between "Completion Date" & "Updated" as "Lastmod"
+    df["Completion Date"] = pd.to_datetime(
+        df["Completion Date"], format="%Y-%m-%d", errors="coerce"
+    )
+    df["Updated"] = pd.to_datetime(
+        df["Updated"], format="%Y-%m-%d %H:%M:%S", errors="coerce"
+    )
+    df["Lastmod"] = (
+        df[["Completion Date", "Updated"]].max(axis=1).dt.strftime("%Y-%m-%d")
+    )
+
+    # Status
+    df["Status"] = df.apply(determine_status, axis=1)
+
+    # Rating, 10/10 not 100/100
+    df["Rating"] = df["Review"] // 10
+    # Replace "Not Rated" with NaN
+    df["Rating"] = df["Rating"].replace(0, np.nan)
+    # Convert to integer while keeping NaN
+    df["Rating"] = pd.to_numeric(df["Rating"], errors="coerce").astype("Int64")
+
+    # Drop the "Review" column
+    df = df.drop("Review", axis=1)
+    # Rename the "Review Notes" column to "Review"
+    df = df.rename(columns={"Review Notes": "Review"})
+
+    # Keep only these columns
+    df = df[
+        [
+            "Title",
+            "Platform",
+            "Storefront",
+            "Status",
+            "Rating",
+            "Date",
+            "Lastmod",
+            "Playtime",
+            "Review",
+        ]
+    ]
+
+    return df
+
+
 # Read CSV file
 file_list = glob.glob("HLTB_Games_*.csv")
 if len(file_list) > 0:
     filepath = file_list[0]
-    df = pd.read_csv(filepath)
     new_file_name = filepath.replace("HLTB_Games_", "HLTB-sanitized-")
+    df = pd.read_csv(filepath)
+    df = sanitized_dataframe(df)
+
+    # Debug preview
+    print(df)
+
+    # Export to CSV
+    df.to_csv(new_file_name, index=False, quoting=1)
 else:
-    print("HLTB exported CSV not found.")
+    print("HLTB exported CSV not found. Please export from options page first.")
     exit()
-
-df = sanitized_dataframe(df)
-
-# Convert to time type
-df[["Progress", "Main Story", "Main + Extras", "Completionist"]] = df[
-    ["Progress", "Main Story", "Main + Extras", "Completionist"]
-].apply(pd.to_timedelta, errors="coerce")
-# Exclude NaN line
-df = df.dropna(
-    subset=["Progress", "Main Story", "Main + Extras", "Completionist"], how="all"
-)
-# Choose the maximum one
-max_playtime = df[["Progress", "Main Story", "Main + Extras", "Completionist"]].max(
-    axis=1
-)
-# Convert back to string as "Playtime"
-max_playtime_hours = max_playtime.dt.total_seconds().div(3600)
-df["Playtime"] = max_playtime_hours.apply(
-    lambda x: "{:02d}:{:02d}:{:02d}".format(
-        int(x // 1), int((x % 1) * 60), int((x * 60) % 60)
-    )
-)
-
-# Use "Added" column as "Date"
-df["Added"] = pd.to_datetime(df["Added"], format="%Y-%m-%d %H:%M:%S", errors="coerce")
-df["Date"] = df["Added"].dt.strftime("%Y-%m-%d")
-
-# Choose nearest date between "Completion Date" & "Updated" as "Lastmod"
-df["Completion Date"] = pd.to_datetime(
-    df["Completion Date"], format="%Y-%m-%d", errors="coerce"
-)
-df["Updated"] = pd.to_datetime(
-    df["Updated"], format="%Y-%m-%d %H:%M:%S", errors="coerce"
-)
-df["Lastmod"] = df[["Completion Date", "Updated"]].max(axis=1).dt.strftime("%Y-%m-%d")
-
-# Status
-df["Status"] = df.apply(determine_status, axis=1)
-
-# Rating, 10/10 not 100/100
-df["Rating"] = df["Review"] // 10
-# Replace "Not Rated" with NaN
-df["Rating"] = df["Rating"].replace(0, np.nan)
-# Convert to integer while keeping NaN
-df["Rating"] = pd.to_numeric(df["Rating"], errors="coerce").astype("Int64")
-
-# Drop the "Review" column
-df = df.drop("Review", axis=1)
-# Rename the "Review Notes" column to "Review"
-df = df.rename(columns={"Review Notes": "Review"})
-
-# Keep only these columns
-df = df[
-    [
-        "Title",
-        "Platform",
-        "Storefront",
-        "Status",
-        "Rating",
-        "Date",
-        "Lastmod",
-        "Playtime",
-        "Review",
-    ]
-]
-
-# Debug preview
-print(df)
-
-# Export to CSV
-df.to_csv(new_file_name, index=False, quoting=1)
