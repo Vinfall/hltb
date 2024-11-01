51322c1b0bcde2595fc5bde67ade624054048627
Vinfall <91039000+Vinfall@users.noreply.github.com>
2024-11-01 14:27:28 +0800

    style: lint

--- monthly.sql
+++ monthly.sql
@@ -1,5 +1,13 @@
-SELECT "Title", "Platform", "Storefront", "Status", "Rating", "Playtime"
+SELECT "Title",
+    "Platform",
+    "Storefront",
+    "Status",
+    "Rating",
+    "Playtime"
 FROM "HLTB"
 WHERE "Lastmod" BETWEEN '2024-09-01' AND '2024-09-30'
-    AND ("Finished" IS NULL OR "Finished" BETWEEN '2024-09-01' AND '2024-09-30') -- would get replaced in query.py
+    AND (
+        "Finished" IS NULL
+        OR "Finished" BETWEEN '2024-09-01' AND '2024-09-30'
+    ) -- would get replaced in query.py
 ORDER BY "Playtime" DESC;
