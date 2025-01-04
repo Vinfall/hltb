dd2e6ae8f61dbcdad0ccc012da74ee7472671189
Vinfall <91039000+Vinfall@users.noreply.github.com>
2025-01-04 17:31:34 +0800

    feat: comprehensive order

--- sql/monthly.sql
+++ sql/monthly.sql
@@ -14,5 +14,15 @@ WHERE "Finished" BETWEEN '2024-09-01' AND '2024-09-30' -- finished this month
             AND NOT "Status" = 'Playing'
         )
     ) -- would get replaced in query.py
-ORDER BY "Playtime" DESC,
-    "Rating" DESC;
+ORDER BY LENGTH("Playtime") DESC,
+    -- 100:00:00 > 99:59:59
+    "Playtime" DESC,
+    "Rating" DESC,
+    CASE
+        WHEN "Status" = "Completed" THEN 1
+        WHEN "Status" = 'Playing' THEN 2
+        WHEN "Status" = 'Stalled' THEN 3
+        WHEN "Status" = 'Backlog' THEN 4
+        WHEN "Status" = 'Retired' THEN 5
+        ELSE 5 -- status rank
+    END;
--- sql/yearly.sql
+++ sql/yearly.sql
@@ -5,19 +5,13 @@ SELECT "Title",
     "Rating",
     "Playtime"
 FROM "HLTB"
-WHERE (
-        "Platform" in ("XSS", "X360", "Xbox")
-        OR "Storefront" in ("EA Play", "Microsoft", "XGP")
-    )
-    AND (
-        "Finished" BETWEEN '2024-01-01' AND '2024-12-31' -- finished this year
-        OR (
-            "Finished" IS NULL
-            AND NOT "Date" > '2024-12-31' -- Playing/Retired/Stalled
-            AND NOT (
-                "Date" < '2024-01-01' -- excluded earlier games
-                AND NOT "Status" = 'Playing'
-            )
+WHERE "Finished" BETWEEN '2024-01-01' AND '2024-12-31' -- finished this year
+    OR (
+        "Finished" IS NULL
+        AND NOT "Date" > '2024-12-31' -- Playing/Retired/Stalled
+        AND NOT (
+            "Date" < '2024-01-01' -- excluded earlier games
+            AND NOT "Status" = 'Playing'
         )
     )
 ORDER BY LENGTH("Playtime") DESC,
